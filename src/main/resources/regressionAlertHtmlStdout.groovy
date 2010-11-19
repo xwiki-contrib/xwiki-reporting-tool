5ffdf31a9cd471ab47862c0ec74dd55a1b4e5d4c
Caleb James DeLisle <calebdelisle@lavabit.com>
2010-11-19 18:11:24 +0000

    [reporter] changed configuration to use internal email publisher.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/contrib/projects/xwiki-reporting-tool@33062 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/resources/regressionAlertHtmlStdout.groovy
+++ src/main/resources/regressionAlertHtmlStdout.groovy
@@ -6,14 +6,16 @@ import org.xwiki.tools.reporter.publishers.*;
 // Where to scrape the data from.
 hudsonURL = "http://hudson.xwiki.org/";
 
+final HtmlMailPublisher emailPub = new HtmlMailPublisher();
+emailPub.addRecipient(System.getProperty("sendMailTo"));
+emailPub.setMailConfig(new HashMap<String, String>() {{
+    put("from", "build.noreply@xwiki.org");
+    put("port", "25");
+    put("host", System.getProperty("mailHost"));
+}});
+
 new Reporter(hudsonURL) {{
-    runReport(new RegressionAlertReport(new StdoutPublisher() {
-        public void publish(final String subject, final String content)
-        {
-            super.publish(subject, content);
-            // Best way I can think of to prevent sending of empty emails every time there are no regressions.
-            throw new RuntimeException("This exception is needed to trigger hudson to publish the email.");
-        }
-    }, Format.HTML));
+    runReport(new RegressionAlertReport(emailPub, Format.HTML));
 }}.run();
+
 
